e68e6253ec79a96d8634f5648706cff0d9a80e5e
Ryulth <adboy94@khu.ac.kr>
2019-02-20 18:36:02 +0900

    add operation

--- src/chapter7/Operation.java
+++ src/chapter7/Operation.java
@@ -24,5 +24,25 @@ public enum Operation {
     public double apply(double x, double y) {
         return operator.applyAsDouble(x, y);
     }
-
+    public static void main(String[] args) {
+        double x = 11;
+        double y = 13;
+        for (Operation op : Operation.values())
+            System.out.printf("%f %s %f = %f%n",
+                    x, op, y, op.apply(x, y));
+    }
 }
+/*public enum Operation2{
+    PLUS("+"){
+        public double apply(double x, double y) {return x + y;}
+    },
+    MINUS("-"){
+        public double apply(double x, double y) {return x - y;}
+    },
+    TIMES("*"){
+        public double apply(double x, double y) {return x * y;}
+    },
+    DIVIDE("-"){
+        public double apply(double x, double y) {return x - y;}
+    },
+}*/
